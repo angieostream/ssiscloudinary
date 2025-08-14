ddbae8078b465791281a0f0e68be21ff3e3d1064
Angie <angelyn.jimeno@g.msuiit.edu.ph>
2025-08-14 13:48:12 +0800

    missed cloudinary config

--- web/extensions.py
+++ web/extensions.py
@@ -3,10 +3,4 @@ import cloudinary
 
 mysql = MySQL()
 
-def init_cloudinary():
-    cloudinary.config( 
-        cloud_name = "dygsi9hr5", 
-        api_key = "259755552848519", 
-        api_secret = "w-i9j_axYRmHkjkQVgZLJt1EQfk", 
-        secure=True
-    )
+
